3c7d516c4d75cc2c580b642228ff27578ecff24b
mayankchaudhary9 <chaudharymayank5800@gmail.com>
2025-12-22 23:56:45 +0530

    ....

--- server/index.js
+++ server/index.js
@@ -2,6 +2,14 @@ import express from "express";
 
 const app = express();
 
+app.use("/api/auth");
+app.use("/api/create");
+app.use("/:id");
+
 app.listen(5000, () => {
   console.log("Server is connected on port: 5000");
 });
+
+// routes we needs
+// GET - Redirection
+// POST - Create short url
